b12948b62ead82c7817e1b3d88039e744cb2b83f
Yuki <paselan@gmail.com>
2018-10-17 18:34:55 +0900

    Select all fields in urls table

--- history/history.go
+++ history/history.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"net/url"
 	"path/filepath"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/pasela/alfred-chrome-history/profile"
@@ -16,8 +17,12 @@ const (
 )
 
 type Entry struct {
-	URL   string
-	Title string
+	URL           string
+	Title         string
+	VisitCount    int
+	TypedCount    int
+	LastVisitTime time.Time
+	Hidden        int
 }
 
 type History struct {
@@ -62,11 +67,12 @@ func (h *History) Query(url, title string) ([]Entry, error) {
 
 	rows, err := h.db.Query(`
 		SELECT
-			url, title
+			url, title, visit_count, typed_count, last_visit_time, hidden
 		FROM
 			urls
 		WHERE
-			title LIKE ? ESCAPE ? OR url LIKE ? ESCAPE ?
+			(title LIKE ? ESCAPE ? OR url LIKE ? ESCAPE ?)
+			AND hidden = 0
 		ORDER BY
 			visit_count DESC, typed_count DESC, last_visit_time DESC
 	`, u, string(escapeChar), t, string(escapeChar))
@@ -78,9 +84,19 @@ func (h *History) Query(url, title string) ([]Entry, error) {
 	entries := make([]Entry, 0)
 	for rows.Next() {
 		var entry Entry
-		if err := rows.Scan(&entry.URL, &entry.Title); err != nil {
+		var visit int64
+		err := rows.Scan(
+			&entry.URL,
+			&entry.Title,
+			&entry.VisitCount,
+			&entry.TypedCount,
+			&visit,
+			&entry.Hidden,
+		)
+		if err != nil {
 			return nil, err
 		}
+		entry.LastVisitTime = convertChromeTime(visit)
 		entries = append(entries, entry)
 	}
 	if err := rows.Err(); err != nil {
@@ -88,4 +104,14 @@ func (h *History) Query(url, title string) ([]Entry, error) {
 	}
 
 	return entries, nil
+}
+
+// https://code.google.com/p/chromium/codesearch#chromium/src/base/time/time.h
+func convertChromeTime(msec int64) time.Time {
+	sec := msec / 1000000
+	nsec := (msec % 1000000) * 1000
+
+	t := time.Unix(sec, nsec)
+	t = t.AddDate(-369, 0, 0) // 369 = 1970 - 1601
+	return t
 }
