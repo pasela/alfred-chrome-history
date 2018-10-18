9f2074ca00f6a6f77d8840079f2c393125b3f678
Yuki <paselan@gmail.com>
2018-10-18 15:45:45 +0900

    Add --alfred option for query command

--- commands/query.go
+++ commands/query.go
@@ -5,13 +5,22 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strconv"
 
+	aw "github.com/deanishe/awgo"
 	"github.com/pasela/alfred-chrome-history/history"
 	"github.com/pasela/alfred-chrome-history/utils"
 	"github.com/spf13/cobra"
 )
 
+type queryOptions struct {
+	AlfredFlag bool
+}
+
+var queryOpts queryOptions
+
 func init() {
+	queryCmd.Flags().BoolVar(&queryOpts.AlfredFlag, "alfred", false, "Alfred script filter mode")
 	rootCmd.AddCommand(queryCmd)
 }
 
@@ -19,10 +28,49 @@ var queryCmd = &cobra.Command{
 	Use:   "query",
 	Short: "query Chrome history",
 	Args:  cobra.MinimumNArgs(1),
-	RunE:  runQueryCmd,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if queryOpts.AlfredFlag {
+			return runQueryCmdAlfred(cmd, args)
+		}
+		return runQueryCmd(cmd, args)
+	},
 }
 
 func runQueryCmd(cmd *cobra.Command, args []string) error {
+	query := args[0]
+	entries, err := runQuery(query, query)
+	if err != nil {
+		return err
+	}
+
+	for _, entry := range entries {
+		fmt.Println(entry)
+	}
+	return nil
+}
+
+func runQueryCmdAlfred(cmd *cobra.Command, args []string) error {
+	wf := aw.New()
+	wf.Run(func() {
+		query := args[0]
+		entries, err := runQuery(query, query)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, entry := range entries {
+			item := wf.NewItem(entry.Title)
+			item.UID(strconv.Itoa(entry.ID))
+			item.Subtitle(entry.URL)
+			item.Arg(entry.URL)
+			item.Valid(true)
+		}
+		wf.SendFeedback()
+	})
+	return nil
+}
+
+func runQuery(url, title string) ([]history.Entry, error) {
 	histFile := historyFile{
 		Profile: globalOpts.Profile,
 		Clone:   false,
@@ -31,26 +79,16 @@ func runQueryCmd(cmd *cobra.Command, args []string) error {
 
 	filePath, err := histFile.GetPath()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	his, err := history.Open(filePath)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer his.Close()
 
-	query := args[0]
-	entries, err := his.Query(query, query)
-	if err != nil {
-		return err
-	}
-
-	for _, entry := range entries {
-		fmt.Println(entry)
-	}
-
-	return nil
+	return his.Query(url, title)
 }
 
 type historyFile struct {
